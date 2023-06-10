6f10453eefeca9d872c23c81854b1b0cfda9fae0
s0ojin <onepst@hanyang.ac.kr>
2023-06-10 19:21:34 +0900

    Setting : lint error 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 function App() {
   return (
     <div className="text-3xl font-bold underline">
       토스트 팝업을 만들어볼까요?
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
