018f06b3235fb21a890930062f9d1c81c32c2ae9
s0ojin <onepst@hanyang.ac.kr>
2023-06-11 02:19:18 +0900

    Setting : 기본스타일 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'toast-bottom':
+          'toast-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'toast-top':
+          'toast-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+      },
+    },
+    colors: {
+      black: {
+        100: ' #000000',
+        50: '#949494',
+        30: '#E7E7E7',
+        10: '#F5F5F5',
+      },
+      white: '#FFFFFF',
+    },
+    fontSize: {
+      Title: ['32px', { fontWeight: '700' }],
+      SubTitle: ['20px', { fontWeight: '500' }],
+      Body: [
+        '16px',
+        { fontWeight: '400', lineHeight: '20.5px', letterSpacing: '-0.3px' },
+      ],
+    },
   },
   plugins: [],
-}
+};
