143d38151dc230d23c25c3a10048aa5759528407
s0ojin <onepst@hanyang.ac.kr>
2023-06-13 14:20:16 +0900

    Fix : 프로그래스바 깜빡임 오류 수정

--- src/toast/ProgressBar.tsx
+++ src/toast/ProgressBar.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function ProgressBar({ delay }: { delay: string | null }) {
   const startTime = new Date();
   document.documentElement.style.setProperty('--progress', '1');
+  const progressBarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const timer = setInterval(() => {
       const currentTime = new Date();
       const passedTime = currentTime.getTime() - startTime.getTime();
-      document.documentElement.style.setProperty(
+      progressBarRef.current?.style.setProperty(
         '--progress',
         String(1 - passedTime / Number(delay)),
       );
@@ -20,7 +21,10 @@ function ProgressBar({ delay }: { delay: string | null }) {
     <>
       {delay && (
         <div
-          style={{ width: `calc(var(--progress) * 100%)` }}
+          ref={progressBarRef}
+          style={{
+            width: `calc(var(--progress) * 100%)`,
+          }}
           className="absolute inset-x-0 bottom-0 h-[0.6rem] bg-white opacity-60"
         />
       )}
